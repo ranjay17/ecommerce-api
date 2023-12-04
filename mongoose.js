b055721b76dd3f0f28baa20558903fa9859417dc
ranjay17 <145134469+ranjay17@users.noreply.github.com>
2023-12-04 12:32:06 +0530

    Add files via upload

---
+++ mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+// connecting mongoose to its default server and ecommerceDB
+mongoose.connect('mongodb+srv://sranjay15:rrRR1234@cluster0.e5jjfpe.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+db.once('open', function(){
+    console.log('Successfully connected to Database : MongoDB');
+});
+
+module.exports = db;
