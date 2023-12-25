8a0971c204f14e60916db853f5425ee42b8d38d8
Ranjay Singh <Sranjay15@gmail.com>
2023-12-25 22:40:03 +0530

    Update mongoose.js

--- mongoose.js
+++ mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // connecting mongoose to its default server and ecommerceDB
-mongoose.connect('mongodb+srv://sranjay15:rrRR1234@cluster0.e5jjfpe.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://sranjay15:rrRR9006@cluster0.e5jjfpe.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
